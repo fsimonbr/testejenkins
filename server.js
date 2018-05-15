308ddc1e1b032c294c75893d318fe8002ea6cf11
fsimonbr <fsimonbr@gmail.com>
2018-05-15 19:36:09 -0300

    tese

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8081;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/new', (req, res) => {
+  res.send('Hello world - NOVO\n');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
