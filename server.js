d1425cd7e3d75e4290d5e03946f480b193836876
fsimonbr <fsimonbr@gmail.com>
2018-05-16 23:18:50 -0300

    teste

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(function(req, res, next) {
 });
 
 app.get('/new', (req, res) => {
-  res.send('Hello Docker Swarm - COMPASSO\n');
+  res.send('Hello Docker Swarm - COMPASSO - NOVO\n');
 });
 
 app.listen(PORT, HOST);
